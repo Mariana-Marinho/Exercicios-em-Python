69fd88daac72d10db5daee771c27638409b1350c
Mariana Marinho <mariana.marinhoa@gmail.com>
2022-08-15 23:13:56 -0300

    exercícios otimizados: 106

---
+++ aulas/aula022.py
@@ -0,0 +1,19 @@
+"""
+Módulos e pacotes
+    Modularização:
+        dividir um programa grande, aumentar a legibilidade e facilitar a manutenção
+        file com funções, definições, listas globais etc para o funcionamento do main
+        quando chamar no main, com import, tentar chamar só com import
+            import dicionário
+        por que se tiver duplicidade no nome das funções pode haver erros, o python executará a função do último dict
+        vantagens:
+            organização
+            facilidade de manutenção
+            oculta o código detalhado
+            reutilização em outros projetos
+
+    Pacotes:
+        pasta que contém módulos, divididos por assunto
+        pasta de pacotes recebe os arquivos por assunto
+
+"""
--- exemplos/ex106.py
+++ exemplos/ex106.py
@@ -1,26 +1,33 @@
 """
 Mini sistema que use o help(), o usuário vai digitar o comando e o manual vai aparecer, termina quando digita 'FIM'
 """
+from time import sleep
+cores = ('\033[m', '\033[44m', '\033[46m', '\033[45m')
 
 
-def bloco(frase, cor='\033[m'):
+def bloco(frase='', cor=0):
     tamanho = len(frase)+4
-    print(cor)
+    print(cores[cor])
     print('_'*tamanho)
-    print(f'  {frase}')
+    print(frase)
     print('_'*tamanho)
-    print(f'\033[m')
+    print(cores[0])
+    sleep(1)
 
 
 def manual(comando):
-    bloco(f'{help(comando)}', '\033[46m')
+    bloco(f'acessando o manual do comando {comando}', 2)
+    print(cores[1])
+    help(comando)
+    print(cores[0])
+    sleep(2)
 
 
 while True:
+    bloco('HELP - digite FIM para terminar', 3)
     funcao = input('função: ')
     if funcao.upper().strip() == 'FIM':
         print('FIM')
         break
     else:
-        bloco('ACESSANDO O MANUAL', '\033[45m')
         manual(funcao)
