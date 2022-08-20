71bccf52b0f3af5806ea9c2f02d89badb2a574a8
Mariana Marinho <mariana.marinhoa@gmail.com>
2022-08-19 23:28:52 -0300

    exercícios feitos: 115a exercícios otimizados: 115

--- exemplos/ex115/cadastros.py
+++ exemplos/ex115/cadastros.py
@@ -1,5 +1,15 @@
+# importando a função leia_int()
+from exemplos.ex113 import *
+
+
 def linhas():
     print('_'*30)
+
+
+def bloco(frase):
+    linhas()
+    print(f'{frase:^30}')
+    linhas()
 
 
 def cadastro():
@@ -13,7 +23,7 @@ def cadastro():
             elif escolha == 2:
                 cadastrar(cadastrados)
             elif escolha == 3:
-                print('Programa finalizado')
+                print(f'\n\033[31mPrograma finalizado\033[m')
                 break
         except ValueError:
             print('digite um valor válido')
@@ -22,33 +32,28 @@ def cadastro():
 
 
 def menu():
-    linhas()
-    print(f'{"MENU PRINCIPAL":^30}')
-    linhas()
+    print()
+    bloco('MENU PRINCIPAL')
     print(f"""\033[36m    1 - Ver pessoas cadastradas
     2 - Cadastrar nova pessoa
     3 - Sair do sistema\033[m""")
     linhas()
-    escolha = input('\033[35mSua opção: \033[m')
+    escolha = leia_int('\033[35mSua opção: \033[m')
     linhas()
     return escolha
 
 
 def pessoas_cadastradas(lista):
-    linhas()
-    print(f'{"PESSOAS CADASTRADAS":^30}')
-    linhas()
+    bloco('PESSOAS CADASTRADAS')
     for p in lista:
-        for i, v in p.items():
-            print(f'{i:<10}{v:>20}')
+        print(f'{p[0]:<10}{p[1]:>20}')
 
 
 def cadastrar(lista):
+    pessoa = []
     nome = input('nome: ')
     idade = input('idade: ')
-    pessoa = {
-        'nome': nome,
-        'idade': idade
-    }
+    pessoa.append(nome)
+    pessoa.append(idade)
     lista.append(pessoa.copy())
     pessoa.clear()
